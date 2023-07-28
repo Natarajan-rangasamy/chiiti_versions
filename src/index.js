6cc30aebfdbe55f3df5ed55290014a21657b1df9
Natarajan-rangasamy <natraj2431@gmail.com>
2023-07-28 12:59:05 +0530

    packege update

--- src/index.js
+++ src/index.js
@@ -6,11 +6,11 @@ import reportWebVitals from "./reportWebVitals";
 import App from "./App";
 
 
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
       <App />
-    
   </React.StrictMode>
 );
 
